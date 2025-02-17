1730f6b2f48145d29190c8b7b9371d6b38091af2
Servando De La Cruz <servandodlcb@gmail.com>
2025-02-17 12:58:53 -0600

    styles(layout).- Ajuste de estilos en layout.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -247,7 +247,7 @@
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
                                 <img class="img-profile rounded-circle"
-                                    src="https://cdn-icons-png.flaticon.com/128/1144/1144709.png">
+                                    src="https://cdn-icons-png.flaticon.com/128/18851/18851112.png">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
