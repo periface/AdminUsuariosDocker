8cb31ae4cf337850403db32524330322fe2ab9e7
periface <temp_peri@hotmail.com>
2025-02-18 18:44:51 -0600

    Sql

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'libsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
